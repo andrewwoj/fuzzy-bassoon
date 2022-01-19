6f94eb3731a67249a1cf54ed49f84003ad748d37
andrewwoj <awoj@lacework.net>
2022-01-19 07:47:45 -0500

    fix a mistake

--- gke-project-01.tf
+++ gke-project-01.tf
@@ -50,8 +50,8 @@ resource "google_container_cluster" "gke-project-01-cluster-01" {
   }
   master_authorized_networks_config  {
     cidr_blocks =  [{
-        cidr_block = "0.0.0.0/0"
-        name = "test-network"
+          cidr_block = "0.0.0.0/0"
+      name = "test-network"
     }]
   }
 }
